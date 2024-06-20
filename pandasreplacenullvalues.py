e2215d37d06ce781cfa86c07f4d3ba49e5dba05f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-20 10:07:55 +0530

    commit

--- pandasreplacenullvalues.py
+++ pandasreplacenullvalues.py
@@ -4,7 +4,12 @@ pd.options.display.max_columns=10
 pd.options.display.max_rows=500
 data=pd.read_csv("C:/Users/KAVYA JOSHI/Downloads/DATASETS/TITANIC.csv")
 #print number of null in particular column
-print
+#print(data.isnull().sum())
+"""
 data["Age"].fillna(54,inplace=True)
 print(data.isnull().sum())
-print(data["Age"])
+print(data["Age"])"""
+#PRINT MEAN MEADIAN NODE OF PARTICULAR COLUMN IN NULL PLACE
+"""a=data["Age"].mean()
+data["Age"].fillna(a,inplace=True)
+print(data["Age"])"""
--- seabornemployeecsv.py
+++ seabornemployeecsv.py
@@ -6,6 +6,9 @@ pd.options.display.max_columns=9
 pd.options.display.max_rows=1499
 a=pd.read_csv("C:/Users/KAVYA JOSHI/Downloads/DATASETS/hr_data.csv")
 print(a.columns)
-sns.lineplot(x="number_project",y="average_montly_hours",data=a)
-plt.title("RESULT GRAPH")
+plt.figure(figsize=(10,5))
+#sns.lineplot(x="number_project",y="average_montly_hours",data=a,hue="department",style="left",legend="full",palette="inferno")
+#plt.title("RESULT GRAPH")
+#plt.show()
+sns.histplot(a["average_montly_hours"],kde=False)
 plt.show()
