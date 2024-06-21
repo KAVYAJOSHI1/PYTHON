138a7c65860a2a107f436a1eadb58e7435eb0c9f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-21 12:25:11 +0530

    commit

--- PANDAS1.py
+++ PANDAS1.py
@@ -20,3 +20,24 @@ print(data.groupby("Department")["Age"].mean().idxmax())
 #Data Manipulation
 data["Bonus"]=data["Salary"]*0.1
 print(data)
+new_data=data[(data["Age"]>30) & (data["Salary"]<80000)]
+print(new_data)
+
+#Sorting data
+#data.sort_values(by="Age",ascending=True)
+a=data.sort_values(by="Salary",ascending=False)
+print(a)
+
+#Handling missing data
+x=data["Age"].mean()
+data["Age"].fillna(x,inplace=True)
+print(data)
+print(data.isnull().sum())
+data.loc["John"]=None
+data.dropna(subset=["Age"],inplace=True)
+print(data)
+
+#Advanced Operations
+print(data[data["Salary"]==data["Salary"].max()])
+print(data["Salary"].sum())
+
