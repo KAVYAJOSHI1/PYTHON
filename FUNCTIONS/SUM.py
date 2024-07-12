b8c72b875882d7b3abb7afa90e9cb9f7e44790a5
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-12 23:44:42 +0530

    a

---
+++ FUNCTIONS/SUM.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    return a+b 
+a=int(input("ENTER THE FIRST NUMBER\n"))
+b=int(input("ENTER THE SECOND NUMBER\n"))
+print("THE SUM OF "+str(a)+" AND "+str(b)+" IS "+str(sum(a,b)))  #sum(a,b)
