256e91eb102a897fd44015f01f5289c579aed74c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-13 23:57:14 +0530

    a

--- calculator.py
+++ calculator.py
@@ -1,33 +1,25 @@
-ch=0
-a=int(input("ENTER VALUE OF A"))
-b=int(input("ENTER VALUE OF B"))
-print("ENTER 1 FOR ADDITION")
-print("ENTER 2 FOR SUBSTRACTION")
-print("ENTER 3 FOR MULTIPLICATION")
-print("ENTER 4 FOR DIVISION")
-ch=int(input())
-
-if ch==1 :
-
-    sum=0
-    sum=a+b
-    print("SUM OF "+str(a)+"AND "+str(b)+"IS "+str(sum))
-
-elif ch==2 :
-
-    sub=a-b
-    print(str(sub))
-
-elif ch==3 :
-
-    mul=a*b
-    print(str(mul))
-    
-elif ch==4 :
-
-    div=a/b
-    print(str(div))
-
-else:
-    
-    print("ENTER FROM 1 TO 4")
+print("WELCOME TO THE CALCULATOR")
+print("ENTER VALUE OF 2 NUMBERS")
+a=int(input())
+b=int(input())
+while True:
+    print("ENTER 1 FOR ADDITION")
+    print("ENTER 2 FOR SUBSTRACTION")
+    print("ENTER 3 FOR MULTIPLICATION")
+    print("ENTER 4 FOR DIVISION")
+    print("ENTER 5 TO EXIT")
+    print("ENTER YOUR CHOICE")
+    ch=int(input())
+    if ch==1:
+        print("THE SUM IS",a+b)
+    elif ch==2:
+        print("THE SUBSTRACTION IS",a-b)
+    elif ch==3:
+        print("THE MULTIPLICATION IS",a*b)
+    elif ch==4:
+        print("THE DIVISION IS",a/b)
+    elif ch==5:
+        print("THANK YOU,QUITTING PROGRAM")
+        break
+    else:
+        print("INVALID CHOICE")
