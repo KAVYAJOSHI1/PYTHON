36d14591c6d5ff7f9dd4ef3265d91e05725269a5
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-15 21:24:26 +0530

    a

--- MATPLOTLIB/GRAPH.py
+++ MATPLOTLIB/GRAPH.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x_axis=np.array([4,5])
-y_axis=np.array([1,2])
-plt.plot(x_axis,y_axis)
+x_axis=np.array([4,5,6,7,8])
+y_axis=np.array([1,2,3,4,5])
+plt.plot(x_axis,y_axis,marker='o')
+plt.title("LINE GRAPH")
+plt.xlabel("X-AXIS")
+plt.ylabel("Y-AXIS")
+plt.grid()
 plt.show()
 
