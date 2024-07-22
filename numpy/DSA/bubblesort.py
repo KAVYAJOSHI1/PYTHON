b25eb276c41044e37563f008b37e34f575dc4fc9
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 20:43:48 +0530

    a

--- numpy/DSA/bubblesort.py
+++ numpy/DSA/bubblesort.py
@@ -1,1 +1,11 @@
+#Bubble sort
 import numpy as np
+print("ENTER THE SIZE OF THE ARRAY:")
+n=int(input())
+list=[]
+print("ENTER THE ELEMENTS:")
+for i in range(0,n):
+    #using numpy
+    list.append(int(input()))
+a=np.array(list)
+    
