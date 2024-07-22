a6319d35c52edb4cfcb8e385d84696214646fc61
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 21:06:16 +0530

    a

--- numpy/DSA/bubblesort.py
+++ numpy/DSA/bubblesort.py
@@ -8,4 +8,14 @@ for i in range(0,n):
     #using numpy
     list.append(int(input()))
 a=np.array(list)
-    
+print("THE ARRAY IS:")
+print(a)
+for i in range(0,n-1):
+    for j in range(i+1,n):
+        if(a[i]>a[j]):
+            temp=a[i]
+            a[i]=a[j]
+            a[j]=temp
+print("THE SORTED ARRAY IS:")            
+print(a)            
+
