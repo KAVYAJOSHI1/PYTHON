88d38c5903bb4ed21aa1e903aaeff8d9d6646b00
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 20:06:07 +0530

    a

--- numpy/DSA/binarysearch.py
+++ numpy/DSA/binarysearch.py
@@ -14,4 +14,20 @@ for i in range(n-1):
             list[j]=temp
 print("THE SORTED ARRAY IS:")            
 print(list)
-    
+f=0
+l=n-1
+print("ENTER THE NUMBER TO BE SEARCHED:")
+x=int(input())
+temp=0
+while(f<=l):
+    mid=int((f+l)/2)
+    if list[mid]==x:
+        temp=1
+        print("THE NUMBER FOUND AT INDEX "+str(mid+1))
+        break
+    elif list[mid]<x:
+        f=mid+1
+    else:
+        l=mid-1
+if temp==0:
+    print("THE NUMBER NOT FOUND")    
