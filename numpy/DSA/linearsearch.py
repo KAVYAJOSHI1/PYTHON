ccbb2a6bebd2e60fc969ec585c07225d9335946c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 19:51:58 +0530

    a

--- numpy/DSA/linearsearch.py
+++ numpy/DSA/linearsearch.py
@@ -1,1 +1,18 @@
 #Linear search
+import numpy as np
+print("Enter the size of array")
+n=int(input())
+LIST=[]
+print("ENTER THE ELEMENTS")
+for i in range(n):
+    LIST.append(int(input()))
+a=np.array(LIST) 
+print("ENTER THE NUMBER TO BE SEARCHED")
+x=int(input())
+for i in range(n):
+    if(a[i]==x):
+        print("THE NUMBER FOUND AT POSITION "+str(i+1))
+        break
+    else:
+        print("THE NUMBER NOT FOUND")   
+
