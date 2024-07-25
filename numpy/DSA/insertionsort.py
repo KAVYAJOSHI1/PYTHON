d45c01bdac087c0bcbb32c20dc00d8c2757ae6f1
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-25 22:58:12 +0530

    a

--- numpy/DSA/insertionsort.py
+++ numpy/DSA/insertionsort.py
@@ -13,7 +13,7 @@ print("ARRAY AFTER INSERTION SORTING IS:")
 for i in range(1,n):
      j=i-1
      temp=a[i]
-     while(a[j]>temp and j>0):
+     while(a[j]>temp and j>=0):
           a[j+1]=a[j]
           j-1
      a[j+1]=temp
