9b5e7231ac3e9aba575f9dacb787c21c2330a516
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-16 22:59:32 +0530

    a

--- MATPLOTLIB/GRAPH.py
+++ MATPLOTLIB/GRAPH.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x_axis=np.array([4,5,6,7,8])
-y_axis=np.array([1,2,3,4,5])
+x_axis=np.array([1,2,3,4,5,6,7,9,10])
+y_axis=np.array([20,40,60,80,100,120,140,160,180,200])
 plt.plot(x_axis,y_axis,marker='o')
 plt.title("LINE GRAPH")
-plt.xlabel("X-AXIS")
-plt.ylabel("Y-AXIS")
+plt.xlabel("labour")
+plt.ylabel("production")
 plt.grid()
 plt.show()
 
